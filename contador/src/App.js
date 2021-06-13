763307f961dfca10d2c39ad2ca718e95651b51cf
Arthur Almeida <68932243+thuralmeida@users.noreply.github.com>
2021-06-13 16:07:58 -0300

    projeto Contador

---
+++ contador/src/App.js
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+
+function Contador() {
+  const [contador, setContador] = useState(1);
+
+  function adicionarContador() {
+    setContador(contador + 1);
+  }
+
+
+
+  return (
+    <div>
+      <div>{contador}</div>
+      <button onClick={adicionarContador}>Adicionar</button>
+    </div>
+  );
+
+}
+
+function Pagina() {
+  return <Contador />
+}
+
+export default Pagina;
